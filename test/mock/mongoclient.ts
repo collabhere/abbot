a9842aec902a2da531d9318d207c56f702f8765d
Abhishek <abhishek.kumar251095@gmail.com>
2020-09-05 16:59:23 +0530

    Tests: unit-tests for prepare & fetch

---
+++ test/mock/mongoclient.ts
@@ -0,0 +1,17 @@
+import { Db } from "mongodb"
+
+export const mockMongoClient = () => ({
+    db: () => ({
+        collection: (collection: string) => ({
+            indexes: () => [{
+                    name: 'TEST_INDEX',
+                    key: {
+                        field_1: 1,
+                        field_2: 1,
+                        field_3: 1,
+                        field_4: -1
+                    }
+                }]
+        })
+    })
+})
