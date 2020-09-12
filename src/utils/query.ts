ef6ef2838b435ed62807ffa52023d6f930eccf1b
Abhishek <abhishek.kumar251095@gmail.com>
2020-09-13 00:52:02 +0530

    Conditions: changes to accomodate  and

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,10 +1,34 @@
 import { IQueryFieldTypes } from "./types";
 
-export const isEquality = (queryFieldVal: any | string | number): boolean =>
-	(typeof queryFieldVal === 'string'
+export const conditionParser = (fieldExpr: {[k:string]: any}) => (fieldExpr.if !== undefined) 
+																? {then: fieldExpr.then, else: fieldExpr.else} 
+                                                                : {then: fieldExpr[1], else: fieldExpr[2]}
+
+
+export const isEquality = (queryFieldVal: any | string | number): boolean => {
+	if (typeof queryFieldVal === 'string'
 		|| typeof queryFieldVal === 'number'
 		|| (typeof queryFieldVal === 'object'
-			&& Object.keys(queryFieldVal)[0] === '$eq')) ? true : false;
+			&& (Object.keys(queryFieldVal)[0] === '$eq'
+				|| Object.keys(queryFieldVal)[0] === '$size'))) {
+
+		return true;
+		
+	} else if (Object.keys(queryFieldVal)[0] === '$expr') {
+		//@todo Other checks for '$expr'
+
+		if (queryFieldVal['$expr']['$cond']) {
+			const condEval = conditionParser(queryFieldVal['$expr']['$cond'])
+			if (isEquality(condEval.then) && isEquality(condEval.else)) {
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	return false;
+}
 
 export const getQueryFieldTypes = (
 	query: any,
