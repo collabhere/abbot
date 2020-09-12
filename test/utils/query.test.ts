6720e971314cf454f60a440a7367a2d456ced62d
Abhishek <abhishek.kumar251095@gmail.com>
2020-09-13 00:52:02 +0530

    Conditions: changes to accomodate 'cond' and 'expr'

--- test/utils/query.test.ts
+++ test/utils/query.test.ts
@@ -1,5 +1,32 @@
 import { expect } from 'chai';
-import { getQueryFieldTypes, isEquality } from '../../src/utils/query';
+import { getQueryFieldTypes, isEquality, conditionParser } from '../../src/utils/query';
+
+describe("ConditionParser", () => {
+
+    it ("parses a query with $cond operator -> array", () => {
+        const condition  = [(1 > 1), 1, 2];
+        const parsedCondition = conditionParser(condition);
+
+        expect(parsedCondition).to.exist;
+        expect(parsedCondition.then).to.eq(1);
+        expect(parsedCondition.else).to.eq(2);
+    });
+
+    it ("parses a query with $cond operator -> object", () => {
+        const condition = {
+            if: (2 > 3),
+            then: 4,
+            else: 3
+        };
+
+        const parsedCondition = conditionParser(condition);
+
+        expect(parsedCondition).to.exist;
+        expect(parsedCondition.then).to.eq(4);
+        expect(parsedCondition.else).to.eq(3);
+    });
+
+});
 
 describe("IsEquality", () => {
 
@@ -17,6 +44,80 @@ describe("IsEquality", () => {
 
         expect(isEqualityField).to.exist;
         expect(isEqualityField).to.eq(false);
+    });
+
+    describe("IsEquality --> $cond and $expr", () => {
+
+        it ("Checks whether a field is equality or range -> both equality fields", () => {
+
+            const field = {
+                $expr: {
+                    $cond: [(2 > 3), 2, 3]
+                }
+            }
+    
+            const isEqualityField = isEquality(field);
+    
+            expect(isEqualityField).to.exist;
+            expect(isEqualityField).to.eq(true);
+    
+        });
+
+        it ("Checks whether a field is equality or range -> both range fields", () => {
+
+            const field = {
+                $expr: {
+                    $cond: [(2 > 3), {$gt: 2}, {$lt: 3}]
+                }
+            }
+    
+            const isEqualityField = isEquality(field);
+    
+            expect(isEqualityField).to.exist;
+            expect(isEqualityField).to.eq(false);
+    
+        });
+
+        it ("Checks whether a field is equality or range -> equality and range fields", () => {
+
+            const field = {
+                $expr: {
+                    $cond: [(2 > 3), 2, {$lt: 3}]
+                }
+            }
+    
+            const isEqualityField = isEquality(field);
+    
+            expect(isEqualityField).to.exist;
+            expect(isEqualityField).to.eq(false);
+    
+        });
+
+        it ("Checks whether a field is equality or range -> condition within a condition", () => {
+
+            const field = {
+                $expr: {
+                    $cond: [
+                        (2 > 3), 
+                        2, 
+                        {
+                            $expr: {
+                                $cond: {
+                                    if: (1 > 2),
+                                    then: 1,
+                                    else: { $size: ['1', '2'] }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+    
+            const isEqualityField = isEquality(field);
+    
+            expect(isEqualityField).to.exist;
+            expect(isEqualityField).to.eq(true);
+        });
     });
 });
 
