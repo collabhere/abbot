8a1cf3a3cf43a3355d6cf240579eda040d412fe3
Shashank Kaul <kaulshashank96@gmail.com>
2020-10-11 18:28:40 +0530

    rewrite(reporter): WIP changes.

--- src/lib/reporter.ts
+++ src/lib/reporter.ts
@@ -1,5 +1,21 @@
 import { writeFileSync } from "fs";
-import { MongoIndexKey } from "../utils/types";
+
+/*
+
+{
+	collection: "",
+	context: "",
+	type: "",
+	id: "",
+	conditions: [],
+	suggestions: [
+		{  }
+	]
+}
+
+*/
+
+const REPORT_SYMBOL = Symbol("__report__")
 
 type ReportOptions = {
 	type: "stdout" | "file";
@@ -7,92 +23,93 @@ type ReportOptions = {
 	path?: string;
 };
 
-export const Reporter = () => ({
-	_report: {},
-	setup: function (collection: string, query: any, sort: any, projection: any) { },
-	insert: function (index: string, item: any) {
-		if (this._report[index])
-			this._report[index].push(item);
-		else
-			this._report[index] = [item];
-	},
-	suggest: function (index: string, type: string, fields?: string[]) {
-		const item = { suggestion: type, fields };
-		this.insert(index, item);
-	},
-	suggestOR: function (index: string, ...suggestions: { type: string; fields: string[]; }[]) {
-		const item = {
-			relation: "OR",
-			suggestions: suggestions.map(({ type, fields }) => ({ suggestion: type, fields }))
-		};
-		this.insert(index, item);
-	},
-	suggestAND: function (index: string, ...suggestions: { type: string; fields: string[]; }[]) {
-		const item = {
-			relation: "AND",
-			suggestions: suggestions.map(({ type, fields }) => ({ suggestion: type, fields }))
-		};
-		this.insert(index, item);
-	},
-	report: function ({ type, format, path }: ReportOptions) {
-		switch (format) {
-			case "json": {
-				const json = JSON.stringify(this._report, null, 4)
-				switch (type) {
-					case "stdout": {
-						console.log(json);
-						break;
-					}
-					case "file": {
-						if (!path) throw new Error("Please provide a `path` value to `.report()` when using `file` type of reporting.");
-						writeFileSync(path, json);
-						break;
-					}
-					default: {
-						throw new Error(`Reporting type not implemented for format: ${format}.`);
-					}
-				}
-				break;
+export const Reporter = (
+	collection: string, query?: any,
+	sort?: any, projection?: any,
+	pipeline?: any[]
+) => {
+	const reporter = {
+		[REPORT_SYMBOL]: {
+			collection,
+			type: "",
+			suggestions: []
+		} as { [k: string]: any },
+		suggest: (index: string, suggestion: string, fields?: string[]) => {
+			if (getReportProp("conditions").length > 0) {
+				setReportProp("suggestions", {
+					index,
+					fields,
+					suggestion,
+				})
+			} else {
+				setReportProp("suggestions", { index, fields, suggestion, })
 			}
-			case "txt": {
-				switch (type) {
-					default: {
-						throw new Error(`Reporting type not implemented for format: ${format}.`);
-					}
-				}
-				break;
-			}
-			default: {
-				throw new Error("Reporting format not implemented.");
+		},
+		context: (ctx: any) => setReportProp("context", JSON.stringify(ctx)),
+		report: ({ type, format, path }: ReportOptions) => {
+			switch (format) {
+				//case "json": {
+				//	const json = JSON.stringify(this._report, null, 4);
+				//	switch (type) {
+				//		case "stdout": {
+				//			console.log(JSON.stringify(json, null, 2));
+				//			break;
+				//		}
+				//		case "file": {
+				//			if (!path)
+				//				throw new Error(
+				//					"Please provide a `path` value to `.report()` when using `file` type of reporting."
+				//				);
+				//			writeFileSync(path, json);
+				//			break;
+				//		}
+				//		default: {
+				//			throw new Error(
+				//				`Reporting type not implemented for format: ${format}.`
+				//			);
+				//		}
+				//	}
+				//	break;
+				//}
+				//case "txt": {
+				//	switch (type) {
+				//		default: {
+				//			throw new Error(
+				//				`Reporting type not implemented for format: ${format}.`
+				//			);
+				//		}
+				//	}
+				//	break;
+				//}
+				//default: {
+				//	throw new Error("Reporting format not implemented.");
+				//}
 			}
 		}
+	};
+	
+	const getReportProp = (prop: string) => reporter[REPORT_SYMBOL][prop];
+	
+	const setReportProp = (prop: string, val: any) => {
+		try {
+			if (reporter[REPORT_SYMBOL][prop] instanceof Array)
+				reporter[REPORT_SYMBOL][prop].push(val);
+			else
+				reporter[REPORT_SYMBOL][prop] = val;
+			return true;
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
+	};
+
+	if (query) {
+		setReportProp("type", "query");
+	} else if (pipeline) {
+		setReportProp("type", "aggregation");
 	}
-});
+
+	return reporter;
+};
 
 export type Reporter = ReturnType<typeof Reporter>;
-
-/*
-	// `_report` structure
-	{
-		index1: [
-			{
-				relation: "OR",
-				suggestions: [
-					{ type, fields }, // SUGGESTION 1
-					{ type, fields } // OR SUGGESTION 2
-				]
-			}
-			, ... { type, fields } // OTHER SUGGESTIONS
-		],
-		index2: [
-			{
-				relation: "AND",
-				suggestions: [
-					{ type, fields } // SUGGESTION 1
-					{ type, fields } // AND SUGGESTION 2
-				]
-			}
-			, ... { type, fields } // OTHER SUGGESTIONS
-		]
-	}
-*/
